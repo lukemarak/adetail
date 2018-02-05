e93af95e6cbd80d43e8571b139e8998a6a1ee416
lukemarak <lukemarak@gmail.com>
2018-02-05 11:42:42 +0530

    added admin.py

--- adetail/pdetail/admin.py
+++ adetail/pdetail/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Configuration, Detail
 
-# Register your models here.
+class ConfigurationAdmin(admin.ModelAdmin):
+	list_display = ['processor','ram', 'hdd', 'smps', 'graphics', 'soundcard', 'rs232', 'parallel', 'lan', 'hddbay', 'wipicard', 'systemos', 'others']
+	prepopulated_fields = {'slug': ('processor','ram', 'hdd', 'smps', 'graphics', 'soundcard', 'rs232', 'parallel', 'lan', 'hddbay', 'wipicard', 'systemos', 'others', )}
+admin.site.register(Configuration, ConfigurationAdmin)
+
+class DetailAdmin(admin.ModelAdmin):
+	list_display = ['customer', 'chasis', 'configuration', 'assembled_on', 'assembled_by']
+	search_fields = ['chasis_serial']
+	list_filter = ['customer', 'assembled_on', 'assembled_by']
+admin.site.register(Detail, DetailAdmin)
+	
+	
+	
